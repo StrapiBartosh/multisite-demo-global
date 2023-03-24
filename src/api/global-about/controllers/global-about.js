417179b00cd0a4fe0d4a27003f3243fb2527d6bc
StrapiBartosh <bart.sakowski@strapi.io>
2023-03-24 13:55:08 -0500

    content types

---
+++ src/api/global-about/controllers/global-about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * global-about controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::global-about.global-about');
---
+++ src/api/global-about/routes/global-about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * global-about router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::global-about.global-about');
